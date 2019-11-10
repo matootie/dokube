3ea28e90a2d07a0b87055e4ca8249a35fd19189d
Mateja Lasan <mateja@lasan.ca>
2019-11-09 21:24:51 -0500

    Remove some lines that would reveal sensitive information.
    For testing purposes, the action would present the contents of a users kubeconfig file. This has been removed. Stay safe, kids.
    
    dist/index.js | 3 ---
    index.js      | 3 ---
    2 files changed, 6 deletions(-)

--- dist/index.js
+++ dist/index.js
@@ -948,7 +948,6 @@ const fs = __webpack_require__(747);
 
 async function run() {
   try {
-    // Get Input Values for future use.
     const token = core.getInput('personalAccessToken', { required: true });
     const cluster = core.getInput('clusterName', { required: true });
     const workdir = process.env['GITHUB_WORKSPACE'];
@@ -959,13 +958,11 @@ async function run() {
     const options = {};
     options.outStream = kubeconfig;
     await exec.exec(`${doctl}/doctl`, ['kubernetes', 'cluster', 'kubeconfig', 'show', cluster, '--access-token', token], options);
-    await exec.exec(`cat ${workdir}/dokubetemp/kubeconfig`);
     var data = fs.readFileSync(`${workdir}/dokubetemp/kubeconfig`, 'utf-8');
     let lines = data.split('\n');
     lines.splice(0,1);
     let formatted = lines.join('\n');
     fs.writeFileSync(`${workdir}/dokubetemp/kubeconfig`, formatted, 'utf-8');
-    await exec.exec(`cat ${workdir}/dokubetemp/kubeconfig`);
     const kubectl = await tc.downloadTool("https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/linux/amd64/kubectl");
     await io.mv(kubectl, `${workdir}/dokubetemp/kubectl`);
     const cachedPath = await tc.cacheDir(`${workdir}/dokubetemp`, 'kubectl', 'v1.16.0');
--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const fs = require('fs');
 
 async function run() {
   try {
-    // Get Input Values for future use.
     const token = core.getInput('personalAccessToken', { required: true });
     const cluster = core.getInput('clusterName', { required: true });
     const workdir = process.env['GITHUB_WORKSPACE'];
@@ -19,13 +18,11 @@ async function run() {
     const options = {};
     options.outStream = kubeconfig;
     await exec.exec(`${doctl}/doctl`, ['kubernetes', 'cluster', 'kubeconfig', 'show', cluster, '--access-token', token], options);
-    await exec.exec(`cat ${workdir}/dokubetemp/kubeconfig`);
     var data = fs.readFileSync(`${workdir}/dokubetemp/kubeconfig`, 'utf-8');
     let lines = data.split('\n');
     lines.splice(0,1);
     let formatted = lines.join('\n');
     fs.writeFileSync(`${workdir}/dokubetemp/kubeconfig`, formatted, 'utf-8');
-    await exec.exec(`cat ${workdir}/dokubetemp/kubeconfig`);
     const kubectl = await tc.downloadTool("https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/linux/amd64/kubectl");
     await io.mv(kubectl, `${workdir}/dokubetemp/kubectl`);
     const cachedPath = await tc.cacheDir(`${workdir}/dokubetemp`, 'kubectl', 'v1.16.0');
