aa98cfec5035abb6cc783a79a0dd8f74e714ec1b
Mateja Lasan <mateja@lasan.ca>
2019-11-15 12:58:06 -0500

    Optimize kubeconfig retrieval
    Cluster credentials are now requested using cURL and the DigitalOcean API v2. A kubeconfig is constructed manually from the response. This in effect acheives two main benefits:
    
    * No need to install doctl; lower run time.
    * Tokens generated from gathering cluster credentials can be set to expire.
    
    A huge thanks to Stephen Edwards for putting this together.
    
    README.md         |     1 +
    action.yml        |     4 +
    dist/index.js     | 41621 +++++++++++++++++++++++++++++++++++++++++---
    index.js          |   146 +-
    package-lock.json |    31 +-
    package.json      |     3 +-
    6 files changed, 38892 insertions(+), 2914 deletions(-)
    
    Squashed commit of the following:
    
    commit fa62efbcdfb0dc3e3c45510e159377ee31992f6c
    Author: Stephen Edwards <sventi555@gmail.com>
    Date:   Wed Nov 13 22:55:01 2019 -0500
    
        updated readme with new parameter
    
    commit 9c17419932cd5faeb1a3b498ea57c7e51a163eaf
    Author: Stephen Edwards <sventi555@gmail.com>
    Date:   Wed Nov 13 22:20:03 2019 -0500
    
        thats some good ol fashioned js right there
    
    commit e0d523b44baf756fbb585a6c26ac62920ff63369
    Author: Stephen Edwards <sventi555@gmail.com>
    Date:   Wed Nov 13 22:19:04 2019 -0500
    
        fs is a thing
    
    commit 443c72e7c46a32cc5c110ec45dae8e0b5868772d
    Author: Stephen Edwards <sventi555@gmail.com>
    Date:   Wed Nov 13 22:17:49 2019 -0500
    
        it is json yes
    
    commit b541e4d43bd621949a5afc9e400931c249fc7e42
    Author: Stephen Edwards <sventi555@gmail.com>
    Date:   Wed Nov 13 22:14:30 2019 -0500
    
        that was a github doodad
    
    commit 8dee4ba3a67b5d66845e13edec0aa2439418733d
    Author: Stephen Edwards <sventi555@gmail.com>
    Date:   Wed Nov 13 22:13:19 2019 -0500
    
        get back here io
    
    commit 7d54517e35e0a3d06cf862adbe19d64a41881655
    Author: Stephen Edwards <sventi555@gmail.com>
    Date:   Wed Nov 13 22:11:59 2019 -0500
    
        first try lets go

--- index.js
+++ index.js
@@ -1,36 +1,134 @@
-const path = require('path');
+const fs =require('fs');
 const process = require('process');
+const request = require('request-promise-native');
 const core = require('@actions/core');
-const tc = require('@actions/tool-cache');
 const exec = require('@actions/exec');
 const io = require('@actions/io');
-const fs = require('fs');
+const tc = require('@actions/tool-cache');
 
 async function run() {
   try {
-    const token = core.getInput('personalAccessToken', { required: true });
-    const cluster = core.getInput('clusterName', { required: true });
-    const workdir = process.env['GITHUB_WORKSPACE'];
-    const doctlTar = await tc.downloadTool("https://github.com/digitalocean/doctl/releases/download/v1.33.1/doctl-1.33.1-linux-386.tar.gz");
-    const doctl = await tc.extractTar(doctlTar, workdir);
-    await io.mkdirP(`${workdir}/dokubetemp`);
-    let kubeconfig = fs.createWriteStream(`${workdir}/dokubetemp/kubeconfig`);
-    const options = {};
-    options.outStream = kubeconfig;
-    await exec.exec(`${doctl}/doctl`, ['kubernetes', 'cluster', 'kubeconfig', 'show', cluster, '--access-token', token], options);
-    
-    // Remove first line of Kubeconfig because it, for some bizzare reason, contains the command that was run "doctl kubernetes cluster ..."
-    var data = fs.readFileSync(`${workdir}/dokubetemp/kubeconfig`, 'utf-8');
-    let lines = data.split('\n');
-    lines.splice(0,1);
-    let formatted = lines.join('\n');
-    fs.writeFileSync(`${workdir}/dokubetemp/kubeconfig`, formatted, 'utf-8');
-    
+    /*
+
+    Required Data:
+      - Personal Access Token
+      - Cluster Name
+
+    Data to Retrieve
+      - Cluster ID
+        - From list of clusters
+      - Region
+        - From list of clusters
+      - Certificate Authority Data
+        - From cluster credentials
+      - Token
+        - From cluster credentials
+
+    1. Request list of clusters, using Personal Access Token.
+    2. Filter cluster by cluster name.
+    3. Request cluster credentials using newly retrieved cluster ID.
+    4. Construct kubeconfig.
+    5. Save kubeconfig.
+    6. Download and install kubectl.
+    7. Cache kubeconfig and kubectl.
+    8. Set KUBECONFIG environment variable.
+    9. Add kubectl to PATH.
+
+    */
+
+    // Set working constants.
+    const DOBaseUrl = 'https://api.digitalocean.com';
+    const workdir = `${process.env['HOME']}/dokubetemp`;
+    await io.mkdirP(workdir);
+
+    // Grab user input.
+    const accessToken = core.getInput('personalAccessToken', { required: true });
+    const clusterName = core.getInput('clusterName', { required: true });
+    const expirationTime = core.getInput('expirationTime', { required: true });
+
+    // Request list of clusters and retrieve cluster ID and region.
+    const listClustersOptions = {
+        baseUrl: DOBaseUrl,
+        uri: '/v2/kubernetes/clusters',
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        },
+        json: true
+    };
+    const clusters = await request(listClustersOptions);
+    const cluster = clusters['kubernetes_clusters'].find(c => c.name === clusterName);
+    if (!cluster) throw new Error(`No cluster found with name: '${clusterName}'`);
+    const clusterID = cluster['id'];
+    const region = cluster['region'];
+
+    // Get cluster credentials.
+    const getCredentialsOptions = {
+        baseUrl: DOBaseUrl,
+        uri: `/v2/kubernetes/clusters/${clusterID}/credentials`,
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        },
+        qs: {
+            expiry: expirationTime
+        },
+        json: true
+    };
+    const credentials = await request(getCredentialsOptions);
+    const server = credentials['server']
+    const certAuthData = credentials['certificate_authority_data'];
+    const kubeconfigToken = credentials['token'];
+
+    // Construct a kubeconfig object.
+    const fullName = `do-${region}-${clusterName}`;
+    const kubeconfig = {
+        apiVersion: 'v1',
+        clusters: [
+            {
+                cluster: {
+                    'certificate-authority-data': certAuthData,
+                    server: server,
+                },
+                name: fullName
+            }
+        ],
+        contexts: [
+            {
+                context: {
+                    cluster: fullName,
+                    user: `${fullName}-admin`
+                },
+                name: fullName
+            }
+        ],
+        'current-context': fullName,
+        kind: 'Config',
+        preferences: {},
+        users: [
+            {
+                name: `${fullName}-admin`,
+                user: {
+                    token: kubeconfigToken
+                }
+            }
+        ]
+    };
+
+    // Save the kubeconfig object.
+    const formattedConfig = JSON.stringify(kubeconfig, null, 4);
+    fs.writeFileSync(`${workdir}/kubeconfig`, formattedConfig);
+
+    // Download and install kubectl.
     const kubectl = await tc.downloadTool("https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/linux/amd64/kubectl");
-    await io.mv(kubectl, `${workdir}/dokubetemp/kubectl`);
-    const cachedPath = await tc.cacheDir(`${workdir}/dokubetemp`, 'kubectl', 'v1.16.0');
+    await io.mv(kubectl, `${workdir}/kubectl`);
+
+    // Cache kubectl and kubeconfig.
+    const cachedPath = await tc.cacheDir(workdir, 'kubectl', 'v1.16.0');
+
+    // Set KUBECONFIG environment variable.
+    core.exportVariable('KUBECONFIG', `${cachedPath}/kubeconfig`);
+
+    // Add kubectl to PATH.
     core.addPath(`${cachedPath}/kubectl`);
-    core.exportVariable('KUBECONFIG', `${cachedPath}/kubeconfig`)
   }
   catch (error) {
     core.setFailed(error.message);
