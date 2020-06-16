2c3844e8d00e7cc9ff4a046af08f871169de9b16
Mateja Lasan <mateja@lasan.ca>
2020-06-16 15:20:20 -0400

    Change install method
    This action should now install the correct version of kubectl for the
    given operating system.

--- index.js
+++ index.js
@@ -1,66 +1,39 @@
-const fs = require('fs');
-const process = require('process');
-const request = require('request-promise-native');
-const core = require('@actions/core');
-const io = require('@actions/io');
-const tc = require('@actions/tool-cache');
+const fs = require("fs");
+const path = require("path");
+const request = require("request-promise-native");
+const core = require("@actions/core");
+const io = require("@actions/io");
+const tc = require("@actions/tool-cache");
+const { kubectlSpec } = require("./utils");
 
 async function run() {
   try {
-    /*
-
-    Required Data:
-      - Personal Access Token
-      - Cluster Name
-
-    Data to Retrieve
-      - Cluster ID
-        - From list of clusters
-      - Region
-        - From list of clusters
-      - Certificate Authority Data
-        - From cluster credentials
-      - Token
-        - From cluster credentials
-
-    1. Request list of clusters, using Personal Access Token.
-    2. Filter cluster by cluster name.
-    3. Request cluster credentials using newly retrieved cluster ID.
-    4. Construct kubeconfig.
-    5. Save kubeconfig.
-    6. Download and install kubectl.
-    7. Cache kubeconfig and kubectl.
-    8. Set KUBECONFIG environment variable.
-    9. Add kubectl to PATH.
-
-    */
-
     // Set working constants.
-    const DOBaseUrl = 'https://api.digitalocean.com';
-    const workdir = `${process.env['HOME']}/dokubetemp`;
+    const DOBaseUrl = "https://api.digitalocean.com";
+    const workdir = `${process.env.HOME}/dokubetemp`;
     await io.mkdirP(workdir);
 
     // Grab user input.
-    const accessToken = core.getInput('personalAccessToken', { required: true });
-    const clusterName = core.getInput('clusterName', { required: true });
-    const expirationTime = core.getInput('expirationTime', { required: true });
-    const namespaceName = core.getInput('namespace', { required: true });
-    const kubectlVersion = core.getInput('version', { required: true });
+    const accessToken = core.getInput("personalAccessToken");
+    const clusterName = core.getInput("clusterName");
+    const expirationTime = core.getInput("expirationTime");
+    const namespaceName = core.getInput("namespace");
+    const kubectlVersion = core.getInput("version");
 
     // Request list of clusters and retrieve cluster ID and region.
     const listClustersOptions = {
       baseUrl: DOBaseUrl,
-      uri: '/v2/kubernetes/clusters',
+      uri: "/v2/kubernetes/clusters",
       headers: {
         Authorization: `Bearer ${accessToken}`
       },
       json: true
     };
     const clusters = await request(listClustersOptions);
-    const cluster = clusters['kubernetes_clusters'].find(c => c.name === clusterName);
-    if (!cluster) throw new Error(`No cluster found with name: '${clusterName}'`);
-    const clusterID = cluster['id'];
-    const region = cluster['region'];
+    const cluster = clusters.kubernetes_clusters.find(c => c.name === clusterName);
+    if (!cluster) throw new Error(`No cluster found with name: "${clusterName}"`);
+    const clusterID = cluster.id;
+    const region = cluster.region;
 
     // Get cluster credentials.
     const getCredentialsOptions = {
@@ -75,18 +48,18 @@ async function run() {
       json: true
     };
     const credentials = await request(getCredentialsOptions);
-    const server = credentials['server']
-    const certAuthData = credentials['certificate_authority_data'];
-    const kubeconfigToken = credentials['token'];
+    const server = credentials.server;
+    const certAuthData = credentials.certificate_authority_data;
+    const kubeconfigToken = credentials.token;
 
     // Construct a kubeconfig object.
     const fullName = `do-${region}-${clusterName}`;
     const kubeconfig = {
-      apiVersion: 'v1',
+      apiVersion: "v1",
       clusters: [
         {
           cluster: {
-            'certificate-authority-data': certAuthData,
+            "certificate-authority-data": certAuthData,
             server: server,
           },
           name: fullName
@@ -102,8 +75,8 @@ async function run() {
           name: fullName
         }
       ],
-      'current-context': fullName,
-      kind: 'Config',
+      "current-context": fullName,
+      kind: "Config",
       preferences: {},
       users: [
         {
@@ -119,18 +92,30 @@ async function run() {
     const formattedConfig = JSON.stringify(kubeconfig, null, 4);
     fs.writeFileSync(`${workdir}/kubeconfig`, formattedConfig);
 
-    // Download and install kubectl.
-    const kubectl = await tc.downloadTool(`https://storage.googleapis.com/kubernetes-release/release/v${kubectlVersion}/bin/linux/amd64/kubectl`);
-
     // Set KUBECONFIG environment variable.
-    core.exportVariable('KUBECONFIG', `${workdir}/kubeconfig`);
+    core.exportVariable("KUBECONFIG", `${workdir}/kubeconfig`);
 
-    // Add kubectl to PATH.
-    core.addPath(`${kubectl}`);
+    // Download and install kubectl.
+    const spec = kubectlSpec(kubectlVersion);
+    const kubectlDirectory = tc.find(
+      "kubectl",
+      kubectlVersion,
+      spec.architecture);
+    if (!kubectlDirectory) {
+      const kubectl = await tc.downloadTool(spec.url);
+      const kubectlDirectory = await tc.cacheFile(
+        path.dirname(kubectl),
+        spec.executable,
+        "kubectl",
+        kubectlVersion,
+        spec.architecture
+      );
+    }
+    core.addPath(kubectlDirectory);
   }
   catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
